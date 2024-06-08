0cd5c0561266151685b53984f3e45248f7a395ae
Misha Grinko <glukym@gmail.com>
2024-06-08 10:44:38 +0300

    00-start

---
+++ README.md
@@ -0,0 +1,15 @@
+# Redux Toolkit Topic
+
+This repo contains code of `Redux Toolkit` lesson.
+Choose a branch with a step you need.
+
+To run the project:
+- use Node.js 20 or later `nvm use 20`;
+- run `npm i` to install deps;
+- run `npm run dev` to start dev server.
+
+```shell
+nvm use 20
+npm i
+npm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Amount } from './components/Amount';
+import { GoodsList } from './components/GoodsList';
+import { Position } from './components/Position';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Amount />
+      <hr />
+      <Position />
+      <hr />
+      <GoodsList />
+    </div>
+  );
+};
